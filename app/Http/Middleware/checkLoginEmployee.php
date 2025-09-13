14317f6a66d65914116a953ce490ab32ffc36d91
adisan103-asusvivobook_x421 <arwinsmith01@gmail.com>
2025-09-13 17:59:40 +0800

    test

--- app/Http/Middleware/checkLoginEmployee.php
+++ app/Http/Middleware/checkLoginEmployee.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use App\Models\UserModel;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Crypt;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class CheckLoginEmployee
